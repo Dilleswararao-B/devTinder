9d728da5b8306daba360f8f98ab4c3c7bd88a5c6
Dilleswararao-B <dilleswararao778@gmail.com>
2026-02-10 10:08:29 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello, Devtinder!");
-})
+
+
+app.use("/user",(req,res,next)=>{
+    console.log("Middleware for /user route");
+    next();
+},
+(req,res)=>{
+    res.send("User route");
+}
+);
+
+app.get("/user/:userId/:userName",(req,res)=>{
+    console.log(req.params);
+    res.send("User route");
+});
+
+app.post("/user",(req,res)=>{
+    res.send("User created");
+});
+
+
+
+
 app.use("/test",(req,res)=>{
     res.send("This is a test route.");
-})
-
-app.use("/about",(req,res)=>{
-    res.send("About Devtinder: A platform to connect developers.");
 })
 
 
